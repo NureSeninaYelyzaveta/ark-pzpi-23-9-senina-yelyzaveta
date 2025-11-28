67c8a773415b2ff535f78f61a1b876cf9d0401eb
NureSeninaYelyzaveta <yelyzaveta.senina@nure.ua>
2025-11-28 21:40:48 +0200

    code-examples-code-conventions.c

--- Pract1/pzpi-23-9-senina-yelyzaveta-pract1/code-examples-code-conventions.c
+++ Pract1/pzpi-23-9-senina-yelyzaveta-pract1/code-examples-code-conventions.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+ 
+ #define MAX_SIZE 100
+ 
+ // Функція обчислює суму елементів масиву
+ int calculate_sum(int arr[], int size) {
+     int sum = 0;
+     for (int i = 0; i < size; i++) {
+         sum += arr[i];
+     }
+     return sum;
+ }
 
+ int main() {
+     int numbers[MAX_SIZE] = { 1, 2, 3, 4, 5 };
+     printf("Sum = %d\n", calculate_sum(numbers, 5));
+     return 0;
+ }
